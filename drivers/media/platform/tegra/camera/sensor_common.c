b8e6a06969f523f90ab2c476fc2c0d83ca8c6820
Greg Rowe <growe@d3engineering.com>
2019-04-24 14:06:27 -0400

    added bggr_wdr_pwl to the mode list (as was originally intended with an earlier patch)

--- drivers/media/platform/tegra/camera/sensor_common.c
+++ drivers/media/platform/tegra/camera/sensor_common.c
@@ -208,9 +208,7 @@ static int extract_pixel_format(
 		*format = V4L2_PIX_FMT_SRGGB12;
 	else if (strncmp(pixel_t, "bayer_wdr_pwl_rggb12", size) == 0)
 		*format = V4L2_PIX_FMT_SRGGB12;
-	else if (strncmp(pixel_t, "bayer_grbg12", size) == 0)
-		*format = V4L2_PIX_FMT_SGRBG12;	
-	else if (strncmp(pixel_t, "bayer_wdr_pwl_grbg12", size) == 0)
+	else if (strncmp(pixel_t, "bayer_wdr_pwl_bggr12", size) == 0)
 		*format = V4L2_PIX_FMT_SGRBG12;	
 	else if (strncmp(pixel_t, "bayer_wdr_dol_rggb10", size) == 0)
 		*format = V4L2_PIX_FMT_SRGGB10;
